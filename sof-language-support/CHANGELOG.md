9873008abb06ecb26308326ef760b5d5f5dbbe50
kleinesfilmroellchen <malu.bertsch@gmail.com>
2020-10-07 11:57:39 +0200

    v 0.0.4

--- sof-language-support/CHANGELOG.md
+++ sof-language-support/CHANGELOG.md
@@ -4,8 +4,15 @@
 
 First alpha release, reflecting the language's alpha state.
 
-## 0.0.2
+## 0.0.3
 
 - Fixed several issues with syntax highlighting.
 - Added some keywords and removed others, reflecting the language's evolution.
 - Added basic programmatic language features.
+
+## 0.0.4
+
+- Fixed more syntax highlighting errors.
+- Modified keywords.
+- Added programmatic language feature for modules.
+- Variable symbols are now only retrieved once for global symbols.
--- sof-language-support/src/sof-langfeatures.ts
+++ sof-language-support/src/sof-langfeatures.ts
@@ -10,6 +10,7 @@ const sofBoolPatternRegex: RegExp = /True|False|true|false/gmu;
 const sofIntegerPatternRegex: RegExp = /(\+|-)?(?:((?:0d)?[0-9]+)|(0[xh][0-9a-fA-F]+)|(0o[0-7]+)|(0b[01]+)|0)/gmu;
 const sofFunctionDefinitionRegex: RegExp = /(?:\s+|^)\}\s+((?:\+|-)?(?:(?:(?:0d)?[0-9]+)|(?:0[xh][0-9a-fA-F]+)|(?:0o[0-7]+)|(?:0b[01]+)|0))\s+(function)\s+(\p{L}(?:\p{L}|[0-9_':])*)\s+(def|globaldef)\b/gmu;
 const sofVariableDefinitionRegex: RegExp = /\b(?<!function\s+)(\p{L}(?:\p{L}|[0-9_':])*)\s+(def|globaldef)\b/gmu;
+const sofImportStatementRegex: RegExp = /(?:"((?:[^"]|(?:\\"))*?(?<!\\))")\s+(use)\b/gmu;
 
 const zip = (...rows: any[][]) => [...rows[0]].map((_, c) => rows.map(row => row[c]));
 
@@ -27,7 +28,7 @@ export function activate(context: vscode.ExtensionContext) {
 				functionMatches.push({ fmatch: result, lastIndex: sofFunctionDefinitionRegex.lastIndex });
 			}
 
-			const fsymbols = <Array<vscode.DocumentSymbol>>functionMatches.map(({ fmatch, lastIndex }) => {
+			const fsymbols: Array<vscode.DocumentSymbol> = functionMatches.map(({ fmatch, lastIndex }) => {
 				console.log(fmatch, lastIndex);
 				const argcount: string = fmatch[1], name: string = fmatch[3], deftype: string = fmatch[4];
 				return new vscode.DocumentSymbol(name, `${deftype === 'globaldef' ? 'global' : 'local'} ${argcount} args`,
@@ -44,7 +45,7 @@ export function activate(context: vscode.ExtensionContext) {
 				variableMatches.push({ vmatch: result, lastIndex: sofVariableDefinitionRegex.lastIndex });
 			}
 
-			const vsymbols = <Array<vscode.DocumentSymbol>>variableMatches.map(({ vmatch, lastIndex }) => {
+			const vsymbols_prelim: Array<vscode.DocumentSymbol> = variableMatches.map(({ vmatch, lastIndex }) => {
 				console.log(vmatch, lastIndex);
 				const name: string = vmatch[1], global: boolean = vmatch[2] === 'globaldef';
 				return new vscode.DocumentSymbol(name, global ? 'global' : 'local',
@@ -53,7 +54,30 @@ export function activate(context: vscode.ExtensionContext) {
 					new vscode.Range(document.positionAt(vmatch.index), document.positionAt(lastIndex)));
 			}).filter(x => x !== null);
 
-			return fsymbols.concat(vsymbols);
+			const vsymbols: Array<vscode.DocumentSymbol> = [];
+			vsymbols_prelim.forEach(docSymbol => {
+				if (docSymbol.detail === 'local') {vsymbols.push(docSymbol);}
+				if (!vsymbols.some(otherDocSymbol => otherDocSymbol.name === docSymbol.name)) {vsymbols.push(docSymbol);}
+			});
+
+
+			// import symbols
+			const importMatches: Array<{ imatch: RegExpExecArray, lastIndex: number }> = [];
+			result = null;
+			while ((result = sofImportStatementRegex.exec(text))) {
+				importMatches.push({ imatch: result, lastIndex: sofImportStatementRegex.lastIndex });
+			}
+
+			const isymbols = <Array<vscode.DocumentSymbol>>importMatches.map(({ imatch, lastIndex }) => {
+				console.log(imatch, lastIndex);
+				const module: string = imatch[1];
+				return new vscode.DocumentSymbol(module, module.startsWith('.') ? 'relative' : 'absolute',
+					vscode.SymbolKind.Module,
+					new vscode.Range(document.positionAt(imatch.index), document.positionAt(lastIndex)),
+					new vscode.Range(document.positionAt(imatch.index + imatch[0].indexOf(module)), document.positionAt(imatch.index + imatch[0].indexOf(module) + module.length)));
+			}).filter(x => x !== null);
+
+			return fsymbols.concat(vsymbols).concat(isymbols);
 		}
 	});
 }
